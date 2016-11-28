ab45bbf36ad729de0139912231f7b90d8d6ff8d5
gness1804 <graham.nessler@gmail.com>
2016-11-28 14:26:34 -0700

    basic grocery item is rendering

--- src/Grocery.js
+++ src/Grocery.js
@@ -1,1 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+const Grocery = ({ name, quantity, notes, purchased, starred, onPurchase, onStar, onDelete}) => {
+  return (
+    <article className="Grocery">
+      <h3>{name}</h3>
+    </article>
+  );
+};
+
+
+export default Grocery;
