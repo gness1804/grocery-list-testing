d195a6d301345036673be070d95807027cbfff24
gness1804 <graham.nessler@gmail.com>
2016-11-28 21:55:35 -0700

    creates fake groceries for testing

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import fakeGroceries from './fake-groceries';
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('');
